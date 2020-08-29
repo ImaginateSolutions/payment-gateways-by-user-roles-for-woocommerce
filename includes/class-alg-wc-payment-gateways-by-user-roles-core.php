3180740a6e0ac195533412e4b014d400f511d28c
Dhruvin <shah.dhruvin90@gmail.com>
2020-08-29 23:54:09 +0530

    Version 1.2.3
    Version 1.2.3

--- includes/class-alg-wc-payment-gateways-by-user-roles-core.php
+++ includes/class-alg-wc-payment-gateways-by-user-roles-core.php
@@ -5,94 +5,108 @@
  * @version 1.2.0
  * @since   1.0.0
  * @author  Tyche Softwares
+ * @package pgur
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-if ( ! class_exists( 'Alg_WC_Payment_Gateways_by_User_Roles_Core' ) ) :
-
-class Alg_WC_Payment_Gateways_by_User_Roles_Core {
-
-	/**
-	 * Constructor.
-	 *
-	 * @version 1.0.0
-	 * @since   1.0.0
-	 */
-	function __construct() {
-		if ( 'yes' === get_option( 'alg_wc_payment_gateways_by_user_roles_plugin_enabled', 'yes' ) ) {
-			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways' ), PHP_INT_MAX, 1 );
-		}
-	}
-
-	/**
-	 * get_current_user_roles.
-	 *
-	 * @version 1.2.0
-	 * @since   1.0.0
-	 * @todo    [dev] (maybe) remove `$is_single_role === true` option
-	 */
-	function get_current_user_roles( $is_single_role ) {
-		$current_user = wp_get_current_user();
-		if ( isset( $current_user->roles ) && is_array( $current_user->roles ) && ! empty( $current_user->roles ) ) {
-			return ( $is_single_role ? $this->handle_guest_role( reset( $current_user->roles ) ) : array_map( array( $this, 'handle_guest_role' ), $current_user->roles ) );
-		} else {
-			return ( $is_single_role ? 'guest' : array( 'guest' ) );
-		}
-	}
-
-	/**
-	 * handle_guest_role.
-	 *
-	 * @version 1.2.0
-	 * @since   1.2.0
-	 * @todo    [dev] `super_admin`
-	 */
-	function handle_guest_role( $role ) {
-		return ( '' != $role ? $role : 'guest' );
-	}
-
-	/**
-	 * check_user_roles.
-	 *
-	 * @version 1.2.0
-	 * @since   1.2.0
-	 */
-	function check_user_roles( $customer_roles, $roles_to_check, $is_single_role ) {
-		if ( $is_single_role ) {
-			return in_array( $customer_roles, $roles_to_check );
-		} else {
-			$intersect = array_intersect( $customer_roles, $roles_to_check );
-			return ( ! empty( $intersect ) );
-		}
-	}
-
-	/**
-	 * available_payment_gateways.
-	 *
-	 * @version 1.2.0
-	 * @since   1.0.0
-	 */
-	function available_payment_gateways( $_available_gateways ) {
-		$is_single_role = ( 'first' === get_option( 'alg_wc_payment_gateways_by_user_roles_check_roles', 'all' ) );
-		$customer_roles = $this->get_current_user_roles( $is_single_role );
-		foreach ( $_available_gateways as $key => $gateway ) {
-			$include_roles = get_option( 'alg_wc_gateway_roles_in_' . $key, '' );
-			if ( ! empty( $include_roles ) && ! $this->check_user_roles( $customer_roles, $include_roles, $is_single_role ) ) {
-				unset( $_available_gateways[ $key ] );
-				continue;
-			}
-			$exclude_roles = get_option( 'alg_wc_gateway_roles_ex_' . $key, '' );
-			if ( ! empty( $exclude_roles ) && $this->check_user_roles( $customer_roles, $exclude_roles, $is_single_role ) ) {
-				unset( $_available_gateways[ $key ] );
-				continue;
-			}
-		}
-		return $_available_gateways;
-	}
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
+if ( ! class_exists( 'Alg_WC_Payment_Gateways_By_User_Roles_Core' ) ) :
+
+	/**
+	 * Core Class.
+	 */
+	class Alg_WC_Payment_Gateways_By_User_Roles_Core {
+
+		/**
+		 * Constructor.
+		 *
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 */
+		public function __construct() {
+			if ( 'yes' === get_option( 'alg_wc_payment_gateways_by_user_roles_plugin_enabled', 'yes' ) ) {
+				add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways' ), PHP_INT_MAX, 1 );
+			}
+		}
+
+		/**
+		 * Get Current User Roles.
+		 *
+		 * @param bool $is_single_role Check if its a single role.
+		 * @version 1.2.0
+		 * @since   1.0.0
+		 * @todo    [dev] (maybe) remove `$is_single_role === true` option
+		 */
+		public function get_current_user_roles( $is_single_role ) {
+			$current_user = wp_get_current_user();
+			if ( isset( $current_user->roles ) && is_array( $current_user->roles ) && ! empty( $current_user->roles ) ) {
+				return ( $is_single_role ? $this->handle_guest_role( reset( $current_user->roles ) ) : array_map( array( $this, 'handle_guest_role' ), $current_user->roles ) );
+			} else {
+				return ( $is_single_role ? 'guest' : array( 'guest' ) );
+			}
+		}
+
+		/**
+		 * Handke Guest User.
+		 *
+		 * @param string $role Role.
+		 * @return bool
+		 * @version 1.2.0
+		 * @since   1.2.0
+		 * @todo    [dev] `super_admin`
+		 */
+		public function handle_guest_role( $role ) {
+			return ( '' != $role ? $role : 'guest' ); //phpcs:ignore
+		}
+
+		/**
+		 * Check User Roles.
+		 *
+		 * @param array $customer_roles Customer Roles.
+		 * @param array $roles_to_check Roles to Check.
+		 * @param bool  $is_single_role If single Role option is enabled.
+		 * @version 1.2.0
+		 * @since   1.2.0
+		 */
+		public function check_user_roles( $customer_roles, $roles_to_check, $is_single_role ) {
+			if ( $is_single_role ) {
+				return in_array( $customer_roles, $roles_to_check ); //phpcs:ignore
+			} else {
+				$intersect = array_intersect( $customer_roles, $roles_to_check );
+				return ( ! empty( $intersect ) );
+			}
+		}
+
+		/**
+		 * Available payment gateways.
+		 *
+		 * @param array $_available_gateways Available Gateways.
+		 * @return array
+		 * @version 1.2.0
+		 * @since   1.0.0
+		 */
+		public function available_payment_gateways( $_available_gateways ) {
+			$is_single_role = ( 'first' === get_option( 'alg_wc_payment_gateways_by_user_roles_check_roles', 'all' ) );
+			$customer_roles = $this->get_current_user_roles( $is_single_role );
+			foreach ( $_available_gateways as $key => $gateway ) {
+				$include_roles = get_option( 'alg_wc_gateway_roles_in_' . $key, '' );
+				if ( ! empty( $include_roles ) && ! $this->check_user_roles( $customer_roles, $include_roles, $is_single_role ) ) {
+					unset( $_available_gateways[ $key ] );
+					continue;
+				}
+				$exclude_roles = get_option( 'alg_wc_gateway_roles_ex_' . $key, '' );
+				if ( ! empty( $exclude_roles ) && $this->check_user_roles( $customer_roles, $exclude_roles, $is_single_role ) ) {
+					unset( $_available_gateways[ $key ] );
+					continue;
+				}
+			}
+			return $_available_gateways;
+		}
+
+	}
 
 endif;
 
-return new Alg_WC_Payment_Gateways_by_User_Roles_Core();
+return new Alg_WC_Payment_Gateways_By_User_Roles_Core();
--- includes/settings/class-alg-wc-payment-gateways-by-user-roles-settings-section.php
+++ includes/settings/class-alg-wc-payment-gateways-by-user-roles-settings-section.php
@@ -5,36 +5,44 @@
  * @version 1.1.0
  * @since   1.0.0
  * @author  Tyche Softwares
+ * @package pgur
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-if ( ! class_exists( 'Alg_WC_Payment_Gateways_by_User_Roles_Settings_Section' ) ) :
-
-class Alg_WC_Payment_Gateways_by_User_Roles_Settings_Section {
-
-	/**
-	 * Constructor.
-	 *
-	 * @version 1.1.0
-	 * @since   1.0.0
-	 */
-	function __construct() {
-		add_filter( 'woocommerce_get_sections_alg_wc_payment_gateways_by_user_roles',              array( $this, 'settings_section' ) );
-		add_filter( 'woocommerce_get_settings_alg_wc_payment_gateways_by_user_roles_' . $this->id, array( $this, 'get_settings' ), PHP_INT_MAX );
-	}
-
-	/**
-	 * settings_section.
-	 *
-	 * @version 1.0.0
-	 * @since   1.0.0
-	 */
-	function settings_section( $sections ) {
-		$sections[ $this->id ] = $this->desc;
-		return $sections;
-	}
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
+if ( ! class_exists( 'Alg_WC_Payment_Gateways_By_User_Roles_Settings_Section' ) ) :
+
+	/**
+	 * Settings Section.
+	 */
+	class Alg_WC_Payment_Gateways_By_User_Roles_Settings_Section {
+
+		/**
+		 * Constructor.
+		 *
+		 * @version 1.1.0
+		 * @since   1.0.0
+		 */
+		public function __construct() {
+			add_filter( 'woocommerce_get_sections_alg_wc_payment_gateways_by_user_roles', array( $this, 'settings_section' ) );
+			add_filter( 'woocommerce_get_settings_alg_wc_payment_gateways_by_user_roles_' . $this->id, array( $this, 'get_settings' ), PHP_INT_MAX );
+		}
+
+		/**
+		 * Settings section.
+		 *
+		 * @param array $sections Section Array.
+		 * @return array
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 */
+		public function settings_section( $sections ) {
+			$sections[ $this->id ] = $this->desc;
+			return $sections;
+		}
+
+	}
 
 endif;
